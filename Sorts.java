32690de10e1d68f9b8d001aee8f8a5e876caa973
Yaying <yliangli20@stuy.edu>
2020-12-16 09:04:36 -0500

    fixed bubbleSort() so no index outta bounds error

--- Sorts.java
+++ Sorts.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class Sorts {
 
   //Bubble sort of an int array.
   //Upon completion, the elements of the array will be in increasing order.
-  //param data  the elements to be sorted.
+  //param data is the elements to be sorted.
   public static void bubbleSort(int[] data) { //doesn't return anything cuz you're modifying the int[]
-    for (int i = 0; i < data.length; i++) { //# of times i need to go thru the array
-      int swap = 0;
-      for (int k = 0; k < data.length-i; k++) { //everytime, i check one less element on the right
+    for (int i = 1; i <= data.length; i++) { //# of times you need to go thru the array
+      System.out.println(Arrays.toString(data));
+      int swap = 0; //number of times elements were swapped in 1 loop of the array
+      for (int k = 0; k < data.length-i; k++) { //everytime, you check one less element on the right
+        //also though, i starts with 1 so you won't get index outta bounds error
         if (data[k] > data[k+1]) {
           int temp = data[k];
           data[k] = data[k+1];
@@ -14,7 +18,7 @@ public class Sorts {
           swap++;
         }
       }
-      if (swap == 0) {
+      if (swap == 0) { //if you never needed to swap elements, means array is sorted
         //break;
         i = data.length;
       }
--- Tester.java
+++ Tester.java
@@ -4,7 +4,13 @@ public class Tester {
   public static void main(String[] args) {
 
     int[] bubsort1 = new int[]{5,1,12,-5,16};
+    int[] bubsort2 = new int[]{2,3,4,5,1};
+    int[] bubsort3 = new int[]{6,1,2,3,4,5};
     Sorts.bubbleSort(bubsort1);
+    Sorts.bubbleSort(bubsort2);
+    Sorts.bubbleSort(bubsort3);
     System.out.println(Arrays.toString(bubsort1));
+    System.out.println(Arrays.toString(bubsort2));
+    System.out.println(Arrays.toString(bubsort3));
   }
 }
